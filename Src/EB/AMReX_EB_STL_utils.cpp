33dfe409cbf0032eecf550831c58589dacfde9f7
Yoo <ejyoo921@gmail.com>
2023-11-10 13:37:33 -0800

    Error of using deleted function in CGAL/AABB_tree.h line 125 -- will try different function

--- Src/EB/AMReX_EB_STL_utils.cpp
+++ Src/EB/AMReX_EB_STL_utils.cpp
@@ -5,7 +5,8 @@
 
 // EY: Timing tool 
 #include <chrono>
-// EY: CGAL
+
+// EY: CGAL --for--Ray-Shooting
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/AABB_tree.h>
@@ -16,21 +17,15 @@
 #include <iostream>
 #include <fstream>
 
-// EY: CGAL -- continued
 typedef CGAL::Simple_cartesian<double> K;
 typedef K::FT FT;
 typedef K::Point_3 Point;
-typedef K::Vector_3 Vector;
+typedef K::Vector_3 Vector3;
 typedef K::Ray_3 Ray;
-typedef K::Triangle_3 Tri;
-
-// The following has a conflict with AABB_face_graph_triangle_primitive
 typedef CGAL::Surface_mesh<Point> Mesh;
 typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
 typedef boost::graph_traits<Mesh>::halfedge_descriptor halfedge_descriptor;
-
 typedef CGAL::AABB_face_graph_triangle_primitive<Mesh> Primitive;
-
 typedef CGAL::AABB_traits<K, Primitive> Traits;
 typedef CGAL::AABB_tree<Traits> Tree;
 typedef boost::optional<Tree::Intersection_and_primitive_id<Ray>::Type> Ray_intersection;
@@ -477,6 +472,21 @@ STLtools::prepare ()
     m_boundry_is_outside = num_isects % 2 == 0;
 }
 
+// EY: For CGAL -- Ray shooting-- connected to STLtools::fill--see below
+struct Skip
+{
+  face_descriptor fd;
+  Skip(const face_descriptor fd)
+    : fd(fd)
+  {}
+  bool operator()(const face_descriptor& t) const
+  { if(t == fd){
+      std::cerr << "ignore " << t  <<std::endl;
+    };
+    return(t == fd);
+  }
+};
+
 void
 STLtools::fill (MultiFab& mf, IntVect const& nghost, Geometry const& geom,
                 Real outside_value, Real inside_value) const
@@ -500,7 +510,8 @@ STLtools::fill (MultiFab& mf, IntVect const& nghost, Geometry const& geom,
 
     // EY: Preparation for CGAL 
     Mesh mesh;
-    // Tree tree(faces(mesh).first, faces(mesh).second, mesh);
+    Tree tree(faces(mesh).first, faces(mesh).second, mesh);
+    double d = CGAL::Polygon_mesh_processing::is_outward_oriented(mesh)?-1:1;
 
     auto const& ma = mf.arrays();
     // EY: Timing the loop
@@ -524,11 +535,31 @@ STLtools::fill (MultiFab& mf, IntVect const& nghost, Geometry const& geom,
         {
             Real pr[]={ptref.x, ptref.y, ptref.z};
             
-            for (int tr=0; tr < num_triangles; ++tr) {
-                if (line_tri_intersects(pr, coords, tri_pts[tr])) {
-                    ++num_intersects;
+            // for (int tr=0; tr < num_triangles; ++tr) {
+            //     if (line_tri_intersects(pr, coords, tri_pts[tr])) {
+            //         ++num_intersects;
+            //     }
+            // }
+
+                for(face_descriptor fd : faces(mesh))
+                {
+                    amrex::Print() << "Using CGAL Ray-Shooting" << "\n";
+                    halfedge_descriptor hd = halfedge(fd,mesh);
+                    Point p = CGAL::centroid(mesh.point(source(hd,mesh)),
+                                            mesh.point(target(hd,mesh)),
+                                            mesh.point(target(next(hd,mesh),mesh)));
+                    Vector3 v = CGAL::Polygon_mesh_processing::compute_face_normal(fd,mesh);
+                    Ray ray(p,d * v);
+                    Skip skip(fd);
+                    Ray_intersection hit = tree.any_intersection(ray, skip);
+                    // if(intersection)
+                    // {
+                    // if(boost::get<Point>(&(intersection->first))){
+                    //     const Point* p =  boost::get<Point>(&(intersection->first) );
+                    //     std::cout <<  *p << std::endl;
+                    // }
+                    // }
                 }
-            }
         }
         ma[box_no](i,j,k) = (num_intersects % 2 == 0) ? reference_value : other_value;
     });
